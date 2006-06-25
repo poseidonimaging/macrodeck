80d38dee71a532ed5c9405562f8027d4dadedd86
Keith Gable <ziggy@ignition-project.com>
2006-06-25 00:33:59 +0000

    - Add a README_FOR_APP so RDoc doesn't crap itself. It doesn't have any valuable info in it. - DataService schema beginnings. - UUIDService.
    git-svn-id: http://svn.macrodeck.com/macrodeck/trunk@18 22037474-b016-0410-8482-8abe5be14c89

---
+++ lib/uuid_service.rb
@@ -0,0 +1,21 @@
+# This service generates UUIDs for other services.
+# It uses the Ruby Gem "uuidtools".
+
+require_gem "uuidtools", ">= 1.0.0"
+
+class UUIDService < BaseService
+	@serviceID = "com.macrodeck.UUIDService"
+	@serviceName = "UUIDService"	
+	@serviceVersionMajor = 1
+	@serviceVersionMinor = 0	
+	@serviceVersionRevision = 20060624
+	@serviceUUID = "05705b50-135a-489f-bbbd-a8fc7d38b643"
+	
+	# Returns a string containing a random UUID.
+	def self.generateUUID()
+		return UUID.random_create.to_s
+	end
+end
+
+# Register this service with Services.
+Services.registerService(UUIDService)
