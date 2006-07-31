d732f3bf4b95b280d0982b6c9456bbb448ac009e
Keith Gable <ziggy@ignition-project.com>
2006-07-31 03:44:43 +0000

    - Fix the MacroDeck permissions so me and Chad can post to the dev blog. - Make the "post a blog" screen have an expanding/contracting post summary box. - Updated style.css to help facilitate this. - Expander/contractor images - schema.rb updated automatically.
    git-svn-id: http://svn.macrodeck.com/macrodeck/trunk@113 22037474-b016-0410-8482-8abe5be14c89

--- db/schema.rb
+++ db/schema.rb
@@ -2,7 +2,7 @@
 # migrations feature of ActiveRecord to incrementally modify your database, and
 # then regenerate this schema definition.
 
-ActiveRecord::Schema.define(:version => 17) do
+ActiveRecord::Schema.define(:version => 18) do
 
   create_table "data_groups", :force => true do |t|
     t.column "groupingtype", :string
