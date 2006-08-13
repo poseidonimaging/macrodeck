b4f2a653be73f49959e311c199cd81aac9f8b7ae
Keith Gable <ziggy@ignition-project.com>
2006-08-13 06:42:59 +0000

    - lookupUUID
    git-svn-id: http://svn.macrodeck.com/macrodeck/trunk@120 22037474-b016-0410-8482-8abe5be14c89

--- vendor/plugins/services/lib/uuid_service.rb
+++ vendor/plugins/services/lib/uuid_service.rb
@@ -8,7 +8,7 @@ class UUIDService < BaseService
 	@serviceID = "com.macrodeck.UUIDService"
 	@serviceName = "UUIDService"	
 	@serviceVersionMajor = 0
-	@serviceVersionMinor = 2
+	@serviceVersionMinor = 3
 	@serviceVersionRevision = 20060705
 	@serviceUUID = "05705b50-135a-489f-bbbd-a8fc7d38b643"
 	
@@ -26,7 +26,36 @@ class UUIDService < BaseService
 		if displayname != nil
 			return displayname
 		else
-			return uuid
+			# Didn't find a user or group, keep truckin'
+			loadedServices = Services.getLoadedServices
+			loadedServices.each do |service|
+				if service[:uuid].downcase == uuid
+					return service[:name]
+				end
+			end
+			# Search Data Items, return data item title
+			ditem = DataItem.find(:first, :conditions => ["dataid = ?", uuid])
+			if ditem != nil
+				return ditem.title
+			else
+				dgroup = DataGroup.find(:first, :conditions => ["grouping = ?", uuid])
+				if dgroup != nil
+					return dgroup.title
+				else
+					ServicesModule::DefaultUUIDs::constants.each do |constant|
+						if ServicesModule::DefaultUUIDs::const_get(constant) == uuid
+							return constant.to_s
+						end
+					end
+					ServicesModule::LocalUUIDs::constants.each do |constant|
+						if ServicesModule::LocalUUIDs::const_get(constant) == uuid
+							return constant.to_s
+						end
+					end
+					# Sorry folks!
+					return uuid
+				end
+			end
 		end
 	end
 end
