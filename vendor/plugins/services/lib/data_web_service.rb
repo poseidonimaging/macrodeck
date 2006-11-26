608fdbfde5bbedc62c5a32aa9d111593c9765230
Keith Gable <ziggy@ignition-project.com>
2006-11-26 06:37:53 +0000

    - Added CreateDataGroup and DeleteDataGroup - Created ReturnUUID return type, returns UUID and error codes - Still using true/false for the functions that would otherwise be a void
    git-svn-id: http://svn.macrodeck.com/macrodeck/trunk@420 22037474-b016-0410-8482-8abe5be14c89

--- vendor/plugins/services/lib/data_web_service.rb
+++ vendor/plugins/services/lib/data_web_service.rb
@@ -1,67 +1,99 @@
 # The DataWebService. Provides DataService functions to
 # remote via ActionWebService.
 
+# Error code constants
+
+DATA_SERVICE_ERROR_OK							= 0
+DATA_SERVICE_ERROR_INVALID_AUTHCODE				= 10
+DATA_SERVICE_ERROR_NO_PERMISSION_TO_WRITE_ITEM	= 100
+DATA_SERVICE_ERROR_NO_PERMISSION_TO_WRITE_GROUP	= 101
+
 # Structs for DataWebService.
 module DataServiceCustomTypes
+	# Metadata for DataItems. Can be nil/null
 	class ItemMetadata < ActionWebService::Struct
 		# creator and owner are inferred by the current user and parent group/item
-		member :dataType,		:string
+		member :dataType,		:string # UUID expected
 		member :title,			:string
 		member :description,	:string
-		member :creatorApp,		:string
+		member :creatorApp,		:string # UUID expected
+	end
+	
+	class GroupMetadata < ActionWebService::Struct
+		member :groupType,		:string # UUID expected
+		member :title,			:string
+		member :description,	:string
 	end
 	
 	class YAML < ActionWebService::Struct
 		member :yamlContent,	:string
 	end
+	
+	class ReturnUUID < ActionWebService::Struct
+		member :errorCode,		:int
+		member :uuid,			:string
+	end
 end
 
 # The DataService API definition
 class DataServiceAPI < ActionWebService::API::Base
+	# DataItem functions
 	api_method :create_data_item, {
-			:expects =>	[{ :authCode	=> :string },
-						 { :grouping	=> :string },
-						 { :metadata	=> DataServiceCustomTypes::ItemMetadata }],
-			:returns => [:string]
-		}
+		:expects =>	[{ :authCode	=> :string },
+					 { :grouping	=> :string },
+					 { :metadata	=> DataServiceCustomTypes::ItemMetadata }],
+		:returns => [{ :returnUUID	=> DataServiceCustomTypes::ReturnUUID }]
+	}
 	api_method :delete_data_item, {
-			:expects =>	[{ :authCode	=> :string },
-						 { :itemUUID	=> :string }],
-			:returns => [:bool]
-		}
+		:expects =>	[{ :authCode	=> :string },
+					 { :itemUUID	=> :string }],
+		:returns => [:bool]
+	}
 	api_method :set_string_value, {
-			:expects =>	[{ :authCode	=> :string },
-						 { :itemUUID	=> :string },
-						 { :value		=> :string }],
-			:returns => [:bool]
-		}
+		:expects =>	[{ :authCode	=> :string },
+					 { :itemUUID	=> :string },
+					 { :value		=> :string }],
+		:returns => [:bool]
+	}
 	api_method :delete_string_value, {
-			:expects => [{ :authCode	=> :string },
-						 { :itemUUID	=> :string }],
-			:returns =>	[:bool]
-		}
+		:expects => [{ :authCode	=> :string },
+					 { :itemUUID	=> :string }],
+		:returns =>	[:bool]
+	}
 	api_method :set_integer_value, {
-			:expects =>	[{ :authCode	=> :string },
-						 { :itemUUID	=> :string },
-						 { :value		=> :int }],
-			:returns => [:bool]
-		}
+		:expects =>	[{ :authCode	=> :string },
+					 { :itemUUID	=> :string },
+					 { :value		=> :int }],
+		:returns => [:bool]
+	}
 	api_method :delete_integer_value, {
-			:expects => [{ :authCode	=> :string },
-						 { :itemUUID	=> :string }],
-			:returns =>	[:bool]
-		}
+		:expects => [{ :authCode	=> :string },
+					 { :itemUUID	=> :string }],
+		:returns =>	[:bool]
+	}
 	api_method :set_object_value, {
-			:expects =>	[{ :authCode	=> :string },
-						 { :itemUUID	=> :string },
-						 { :value		=> DataServiceCustomTypes::YAML }],
-			:returns => [:bool]
-		}
+		:expects =>	[{ :authCode	=> :string },
+					 { :itemUUID	=> :string },
+					 { :value		=> DataServiceCustomTypes::YAML }],
+		:returns => [:bool]
+	}
 	api_method :delete_object_value, {
-			:expects => [{ :authCode	=> :string },
-						 { :itemUUID	=> :string }],
-			:returns =>	[:bool]
-		}
+		:expects => [{ :authCode	=> :string },
+					 { :itemUUID	=> :string }],
+		:returns =>	[:bool]
+	}
+	# DataGroup functions
+	api_method :create_data_group, {
+		:expects => [{ :authCode	=> :string },
+					 { :parent		=> :string },
+					 { :metadata	=> DataServiceCustomTypes::GroupMetadata }],
+		:returns => [{ :returnUUID	=> DataServiceCustomTypes::ReturnUUID }]
+	}
+	api_method :delete_data_group, {
+		:expects => [{ :authCode		=> :string },
+					 { :groupUUID		=> :string }],
+		:returns => [:bool]
+	}	
 end
 
 # The Data Web Service. Provides SOAP/XML-RPC for DataService.
@@ -101,12 +133,12 @@ class DataWebService < ActionWebService::Base
 					creatorapp = metadata[:creatorApp]
 				end
 				uuid = DataService.createData(metadata[:dataType], :nothing, nil, { :creator => user.uuid, :owner => group_md[:owner], :title => title, :description => description, :creatorapp => creatorapp, :grouping => grouping })
-				return uuid
+				return { :errorCode => DATA_SERVICE_ERROR_OK, :uuid => uuid }
 			else
-				return nil
+				return { :errorCode => DATA_SERVICE_ERROR_NO_PERMISSION_TO_WRITE_ITEM, :uuid => nil }
 			end
 		else
-			return nil
+			return { :errorCode => DATA_SERVICE_ERROR_INVALID_AUTHCODE, :uuid => nil }
 		end
 	end
 	
@@ -224,5 +256,57 @@ class DataWebService < ActionWebService::Base
 		else
 			return false
 		end		
+	end
+	
+	# Creates a data group that should be populated with data items. +parent+
+	# is optional and can be nil/null. Returns a fresh UUID if successful.
+	def create_data_group(authCode, parent, metadata)
+		user = UserService.userFromAuthCode(authCode)
+		if user != nil
+			# The user exists! Hooray!
+			
+			# If: 1) No parent was specified, or 2) A parent was specified and they can write to it,
+			# Then: They can get in.
+			if (parent != nil && DataService.canWrite?(parent, user.uuid)) || (parent == nil)
+				# They can write, create the data!
+				if parent != nil
+					parent_md = DataService.getDataGroupMetadata(parent)
+					owner = parent_md[:owner]
+				else
+					owner = user.uuid
+				end
+				if metadata[:title] == ""
+					title = nil
+				else
+					title = metadata[:title]
+				end
+				if metadata[:description] == ""
+					description = nil
+				else
+					description = metadata[:description]
+				end
+				uuid = DataService.createDataGroup(metadata[:groupType], nil, parent, { :creator => user.uuid, :owner => owner, :title => title, :description => description})
+				return { :errorCode => DATA_SERVICE_ERROR_OK, :uuid => uuid }
+			else
+				return { :errorCode => DATA_SERVICE_ERROR_NO_PERMISSION_TO_WRITE_GROUP, :uuid => nil }
+			end
+		else
+			return { :errorCode => DATA_SERVICE_ERROR_INVALID_AUTHCODE, :uuid => nil }
+		end
+	end
+	
+	# Deletes a data group. Leaves orphan data items!
+	def delete_data_group(authCode, groupUUID)
+		user = UserService.userFromAuthCode(authCode)
+		if user != nil
+			if DataService.canWrite?(groupUUID, user.uuid)
+				result = DataService.deleteDataGroup(groupUUID)
+				return result
+			else
+				return false
+			end
+		else
+			return false
+		end	
 	end	
 end
