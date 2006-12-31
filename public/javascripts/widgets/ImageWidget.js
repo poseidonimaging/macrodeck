2516a2b57503ef2bf0f4a202cea452ce62c5fae8
Keith Gable <ziggy@ignition-project.com>
2006-12-31 08:20:16 +0000

    - Import yui-ext images and stylesheet - Convert layout to using yui-ext BorderLayout. I can now have a toolbox! :) - Import a big logo. - ImageComponent - Inklings of an ImageWidget. - Thumbs.db is the devil
    git-svn-id: http://svn.macrodeck.com/macrodeck/trunk@474 22037474-b016-0410-8482-8abe5be14c89

---
+++ public/javascripts/widgets/ImageWidget.js
@@ -0,0 +1,38 @@
+var ImageWidget = Class.create();
+
+ImageWidget.prototype = {
+	initialize:function(instanceId) {
+		this._instanceId = instanceId;
+		this.version = "0.2.20061220";
+		this.author = "Keith Gable <ziggy@ignition-project.com>";
+		this.name = "ImageWidget";
+	},
+	display:function(displayMode) {
+		this._surface = Widgets.getPaintingSurface(this._instanceId);
+		ok = Widgets.createContainer(this._instanceId, "ok", false);
+		cancel = Widgets.createContainer(this._instanceId,
+			"cancel", false);
+		ok_btn = new InputButtonComponent('OK');
+		cancel_btn = new InputButtonComponent('Cancel');
+		
+		// Demo
+		if (displayMode == "demo") {
+			ok_btn.click = function() {
+				alert("You pressed OK!");
+			}
+			cancel_btn.click = function() {
+				alert("You pressed Cancel.");
+			}
+		} else {
+			ok_btn.click = this.okClick;
+			cancel_btn.click = this.cancelClick;
+		}
+		
+		ok_btn.display(ok);
+		cancel_btn.display(cancel);
+		this._surface.appendChild(ok);
+		this._surface.appendChild(cancel);
+	}
+}
+
+Object.extend(ImageWidget.prototype, BaseWidget.prototype);
