ee5690a667e3fe1743b7d8fe84b144b0a8676e7c
Eugene Hlyzov <ehlyzov@issart.com>
2007-01-31 08:38:35 +0000

    search service first commit
    git-svn-id: http://svn.macrodeck.com/macrodeck/trunk@487 22037474-b016-0410-8482-8abe5be14c89

---
+++ vendor/plugins/services/lib/search_service.rb
@@ -0,0 +1,74 @@
+
+class SearchService < BaseService
+
+  def SearchService.search(items,query,where=['title'])
+      fields = parse_where(where)
+      keywords = parse_query(query)
+      itbl = Hash.new
+      inv_search_table = Hash.new
+      search_table = Hash.new
+      items.each {|item|
+          item.attributes(:only=>fields).each {|attr, value|
+              search_table[items.index(item)] = Hash.new
+              search_table[items.index(item)][attr] = Hash.new
+              value.to_s.split.each {|keyword|
+                  if !inv_search_table.has_key?(keyword)
+                      inv_search_table[keyword] = Hash.new
+                      inv_search_table[keyword][attr] = Array.new
+                  elsif !inv_search_table[keyword].has_key?(attr)
+                      inv_search_table[keyword][attr] = Array.new
+                  end
+                  inv_search_table[keyword][attr].push(items.index(item))
+                  search_table[items.index(item)][attr][keyword] = 0
+              }
+              
+          }       
+      }
+       
+  end
+  
+  private
+  
+  def parse_query(query)
+      lex = query.split
+      terms = Array.new
+      ops = Array.new
+      lex.each {|word|
+          case word
+              when "AND"
+                  ops.push("&")
+              when "OR"
+                  ops.push("|")
+              else
+                  terms.push(word)                  
+          end
+      }
+      [lambda_gen(ops),terms]
+  end
+  
+  def parse_where
+      where
+  end
+
+  def lambda_gen(ops)
+      t = Array.new
+      ops.each {|op|
+          t.push(lambda {|t1,t2| t1.send(op,t2)})
+      }
+      lambda {|args|
+          arg1 = args.shift
+          i = 1
+          while !args.empty?
+              arg2 = args.shift
+              arg3 = t[i-1].call(arg1,arg2)
+              i += 1
+              arg1 = arg3
+          end
+          arg1
+      }
+  end
+
+  def relevent_of(data,keywords)
+      data.split
+  end
+end
