774a6b0be3b0e8150e5b2d795f1f5aacf59ec84a
Keith Gable <ziggy@ignition-project.com>
2007-02-19 12:30:27 +0000

    - Please use tabs and don't delete mine, kthx. - Noticed that UUIDs are being duplicated in environment.rb and services/lib/default_uuids.rb. Someone should see if we really need to duplicate them...
    git-svn-id: http://svn.macrodeck.com/macrodeck/trunk@499 22037474-b016-0410-8482-8abe5be14c89

--- config/environment.rb
+++ config/environment.rb
@@ -54,24 +54,26 @@ end
 # Include your application configuration below
 
 # Constants for UUIDs
-USER_CSAKON       = "9add1be8-a635-49d7-aa0f-91b6638e9dd0"
-USER_ZIGGYTHEHAMSTER  = "c4a038aa-372a-4f5c-81b7-351660f7049b"
-GROUP_MACRODECK     = "253d41a1-8b62-4ca8-9f8d-99bb42bc0dd8"
-BLOG_MACRODECK      = "9a9fc352-89d4-4b92-a94b-45a8cac106bb"
-CREATOR_MACRODECK   = "7b7e7c62-0a56-4785-93d5-6e689c9793c9"
+# This moved to services/lib/local_uuids.rb.
+# FIXME: See if we still need these! If not, remove them from here.
+USER_CSAKON				= "9add1be8-a635-49d7-aa0f-91b6638e9dd0"
+USER_ZIGGYTHEHAMSTER	= "c4a038aa-372a-4f5c-81b7-351660f7049b"
+GROUP_MACRODECK			= "253d41a1-8b62-4ca8-9f8d-99bb42bc0dd8"
+BLOG_MACRODECK			= "9a9fc352-89d4-4b92-a94b-45a8cac106bb"
+CREATOR_MACRODECK		= "7b7e7c62-0a56-4785-93d5-6e689c9793c9"
 
 # E-mail validation regex
 #EMAIL_VALIDATION   = /^[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/
 EMAIL_VALIDATION    = /^([a-zA-Z0-9&_?\/`!|#*$^%=~{}+'-]+|"([\x00-\x0C\x0E-\x21\x23-\x5B\x5D-\x7F]|\\[\x00-\x7F])*")(\.([a-zA-Z0-9&_?\/`!|#*$^%=~{}+'-]+|"([\x00-x0C\x0E-\x21\x23-\x5B\x5D-\x7F]|\\[\x00-\x7F])*"))*@([a-zA-Z0-9&_?\/`!|#*$^%=~{}+'-]+|\[([\x00-\x0C\x0E-\x5A\x5E-\x7F]|\\[\x00-\x7F])*\])(\.([a-zA-Z0-9&_?\/`!|#*$^%=~{}+'-]+|\[([\x00-\x0C\x0E-\x5A\x5E-\x7F]|\\[\x00-\x7F])*\]))*$/
 
 # User Password Salt
-PASSWORD_SALT     = "giomullyoxonoind" # Random character generator
+PASSWORD_SALT		= "giomullyoxonoind" # Random character generator
 
 # Default tabs
-DEFAULT_TABS      = [ { :url => "/",                :text => "Home",      :id => "home" },
-              { :url => "/group/macrodeck/blog/",     :text => "Development Blog",:id => "devblog" },
-              { :url => "/directory/",          :text => "Directory",   :id => "directory" },
-              { :url => "http://developer.macrodeck.com/",:text => "Developers",    :id => "developers" }]
+DEFAULT_TABS		= [ { :url => "/",								:text => "Home",			:id => "home" },
+						{ :url => "/group/macrodeck/blog/",			:text => "Development Blog",:id => "devblog" },
+						{ :url => "/directory/",					:text => "Directory",		:id => "directory" },
+						{ :url => "http://developer.macrodeck.com/",:text => "Developers",		:id => "developers" }]
 
 # Start services we need
 Services.startService "uuid_service"
--- vendor/plugins/services/lib/default_uuids.rb
+++ vendor/plugins/services/lib/default_uuids.rb
@@ -14,24 +14,23 @@ module ServicesModule
 
 		# Constants for data groups
 
-		DGROUP_BLOG		= "f7ae8ebd-c49a-4c9c-8f8c-425d32e64d88" # A user/group/site's blog.
-		DGROUP_CALENDAR	= "ae32a6aa-bfb2-4126-87a1-7041da0ce6e5" # A calendar.
-		DGROUP_COMMENTS	= "841d7152-1a50-43c5-b53f-75437faad6a2" # A blog post's comments.
-		DGROUP_PROFILE	= "bf1d3af2-3437-489f-9f09-24151edeb827" # A user/group's profile
+		DGROUP_BLOG			= "f7ae8ebd-c49a-4c9c-8f8c-425d32e64d88" # A user/group/site's blog.
+		DGROUP_CALENDAR		= "ae32a6aa-bfb2-4126-87a1-7041da0ce6e5" # A calendar.
+		DGROUP_COMMENTS		= "841d7152-1a50-43c5-b53f-75437faad6a2" # A blog post's comments.
+		DGROUP_PROFILE		= "bf1d3af2-3437-489f-9f09-24151edeb827" # A user/group's profile
 
 		# Default creator/owner constants
-		NOBODY			= "574ecd0f-afb1-40e4-a71b-1e66622db0de"
-		ANONYMOUS		= "00000000-0000-0000-0000-000000000000"
+		NOBODY				= "574ecd0f-afb1-40e4-a71b-1e66622db0de"
+		ANONYMOUS			= "00000000-0000-0000-0000-000000000000"
 		
 		# Constants for storages
-		STYPE_FOLDER    = "438754d6-227b-4a06-9400-79e941d2fd45" # A folder
-		STYPE_FILE      = "00000000-227b-4a06-9400-79e941d2fd45" # A file
+		STYPE_FOLDER		= "438754d6-227b-4a06-9400-79e941d2fd45" # A folder
+		STYPE_FILE			= "00000000-227b-4a06-9400-79e941d2fd45" # A file
 
         # Constants for subscriptions
-        ONLINE_SUB      = "8c730f9c-a95a-41e7-bd38-ada9277dc6ab" # Online subscription (for something online)
-        OFFLINE_SUB     = "21a2ed06-f315-4941-9af8-a678dbed8858" # Offline subscription (for something offline)
-        WIDGET_SUB      = "666ce30b-2a44-469b-ab29-42a65bfcc337" # Widget subscription
-        ONETIME_PAYMENT = "6ea5818d-dbdf-4d96-95f7-f52a36234ecc" # One-time payment
-
+        ONLINE_SUB			= "8c730f9c-a95a-41e7-bd38-ada9277dc6ab" # Online subscription (for something online)
+        OFFLINE_SUB			= "21a2ed06-f315-4941-9af8-a678dbed8858" # Offline subscription (for something offline)
+        WIDGET_SUB			= "666ce30b-2a44-469b-ab29-42a65bfcc337" # Widget subscription
+        ONETIME_PAYMENT		= "6ea5818d-dbdf-4d96-95f7-f52a36234ecc" # One-time payment
 	end
 end
