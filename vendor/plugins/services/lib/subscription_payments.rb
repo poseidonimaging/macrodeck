ae0de41d3eafd5e34063006acdf909b9d0438843
Eugene Hlyzov <ehlyzov@issart.com>
2007-02-28 22:02:30 +0000

    git-svn-id: http://svn.macrodeck.com/macrodeck/trunk@513 22037474-b016-0410-8482-8abe5be14c89

--- vendor/plugins/services/lib/subscription_payments.rb
+++ vendor/plugins/services/lib/subscription_payments.rb
@@ -6,6 +6,5 @@ class SubscriptionPayment < ActiveRecord::Base
   def get_history(user_uuid, sub_uuid) 
       find_all_by_user_uuid_and_subscription_uuid(user_uuid,sub_uuid, :select=>[:amount_paid,:date_paid])
   end
-  
-  
+      
 end
