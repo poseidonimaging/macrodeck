994ac7f2710ad9d93d187789fbf33c6399a3aaaf
Keith Gable <ziggy@ignition-project.com>
2007-12-12 04:01:07 +0000

    - Patron table - Style tweaks
    git-svn-id: http://svn.macrodeck.com/macrodeck/branches/places-20070719@843 22037474-b016-0410-8482-8abe5be14c89

--- app/helpers/facebook_places_helper.rb
+++ app/helpers/facebook_places_helper.rb
@@ -16,4 +16,23 @@ module FacebookPlacesHelper
 			h[:nid] = network.nid
 			return h
 		end
+
+		# Makes a "Patrons" three column thingy.
+		def patron_table(patrons)
+			table_items = []
+			p_sample = patrons[0..14]
+			p_sample.each do |p|
+				if p.facebook_uid != nil && p.facebook_uid.to_i > 0 
+					table_items << "<fb:name uid=\"#{p.facebook_uid}\" capitalize=\"true\" shownetwork=\"true\" ifcantsee=\"Facebook User ##{p.facebook_uid}\" /><br />"
+				elsif p.name != nil
+					table_items << "#{p.name} (MacroDeck User)"
+				end
+			end
+			return threecol_layout(table_items)
+		end
+
+		# A URL helper that will hopefully override the default fbplaces_url and let you
+		# link to crap easier.
+		def fbplaces_url(options_hash = {})
+		end
 end
