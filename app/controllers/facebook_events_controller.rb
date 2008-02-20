1d129c9d20dd61debf7ed24604a59baf40604213
Keith Gable <ziggy@ignition-project.com>
2008-02-20 16:07:51 +0000

    - Facebook Events controller - do some things
    git-svn-id: http://svn.macrodeck.com/macrodeck/branches/places-20070719@988 22037474-b016-0410-8482-8abe5be14c89

--- app/controllers/facebook_events_controller.rb
+++ app/controllers/facebook_events_controller.rb
@@ -1,2 +1,16 @@
 class FacebookEventsController < ApplicationController
+	# Create an event.
+	# Expects the following URL parameters to exist:
+	# country, state, city, place
+	def create
+		if !params[:country].nil? && !params[:state].nil? && !params[:city].nil? && !params[:place].nil?
+			if params[:create_event].nil?
+				# display create event box
+			else
+				# actually create the event
+			end
+		else
+			raise ArgumentError, "event#create - Required URL component missing"
+		end
+	end
 end
