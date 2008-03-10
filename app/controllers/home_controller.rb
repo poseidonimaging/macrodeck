50ea131881f76de7fa79900daa2167ef3fa75823
Keith Gable <ziggy@ignition-project.com>
2008-03-10 16:25:58 +0000

    - HomeController#sxsw - home/sxsw.rhtml view - not complete - needs two partials ported from Facebook (_hcard, _hevent) - Possibly we should be storing the UUID of the place in a calendar if it's affiliated with a place.
    git-svn-id: http://svn.macrodeck.com/macrodeck/branches/places-20070719@1019 22037474-b016-0410-8482-8abe5be14c89

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,20 @@ class HomeController < ApplicationController
 	def index
 		# static content
 	end
+	
+	# SXSW 08 Austin Events page
+	def sxsw
+	  austincat = Category.find_by_title("Austin")
+	  if austincat
+	    upcoming_events = Calendar.upcoming_events_in_category(austincat.id)
+	    if upcoming_events
+	      # RENDER!
+	      @events = upcoming_events.paginate(:page => params[:page], :per_page => 20)
+	    else
+	      @events = nil
+	    end
+	  else
+	    raise "Austin category does not exist."
+	  end
+	end
 end
