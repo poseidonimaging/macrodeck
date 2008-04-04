7ecdb9117772792f78501a8502ba2fc9beabf2fc
Keith Gable <ziggy@ignition-project.com>
2008-04-04 04:48:34 +0000

    - Patrons are shown by image now (hopefully looks good with lots of people) - Modify hcard_micro and hevent to be more like Chad's mockup (problem is that we don't usually have enough enough width to show the shit on the sides) - Event detail pages are currently broken
    git-svn-id: http://svn.macrodeck.com/macrodeck/branches/places-20070719@1078 22037474-b016-0410-8482-8abe5be14c89

--- app/helpers/facebook_places_helper.rb
+++ app/helpers/facebook_places_helper.rb
@@ -14,16 +14,14 @@ module FacebookPlacesHelper
 
 		# Makes a "Patrons" three column thingy.
 		def patron_table(patrons)
-			table_items = []
+			table_items = ""
 			p_sample = patrons[0..14]
 			p_sample.each do |p|
 				if p.facebook_uid != nil && p.facebook_uid.to_i > 0 
-					table_items << "<fb:name uid=\"#{p.facebook_uid}\" capitalize=\"true\" shownetwork=\"true\" ifcantsee=\"Facebook User ##{p.facebook_uid}\" /><br />"
-				elsif p.name != nil
-					table_items << "#{p.name} (MacroDeck User)"
+					table_items << "<fb:profile-pic uid=\"#{p.facebook_uid}\"linked=\"true\" size=\"square\" />"
 				end
 			end
-			return threecol_layout(table_items)
+			return table_items
 		end
 
 end
