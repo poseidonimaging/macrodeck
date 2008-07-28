8d886fe2bedcd1ea8bf86aafe7d8ad8f2ab5d847
Keith Gable <ziggy@ignition-project.com>
2008-07-28 20:34:46 +0000

    Add a search button to view city and start hooking things up to work
    git-svn-id: http://svn.macrodeck.com/macrodeck/trunk@1169 22037474-b016-0410-8482-8abe5be14c89

--- app/controllers/facebook_search_controller.rb
+++ app/controllers/facebook_search_controller.rb
@@ -1,12 +1,28 @@
 class FacebookSearchController < ApplicationController
+	before_filter :require_facebook_login
+	before_filter :initialize_facebook_user
+	before_filter :setup_breadcrumbs
+
+	layout "facebook_search"
+
 	def search
 		get_networks
 		get_home_city
 		get_secondary_city
 
-		if !params[:country].nil? && !params[:state].nil? && !params[:city].nil?
+		if !params[:uuid].nil?
+			@object = DataObject.find_by_uuid(params[:uuid])
 			if params[:query]
 				# render search results
+				@query = "#{params[:query]}"
+				@search = Ultrasphinx::Search.new(
+					:query => @query,
+					:sort_mode => "relevance",
+					:filters => { "parent_id" => @object.id },
+					:page => params[:page],
+					:per_page => 30
+				)
+				@search.excerpt
 			else
 				# render search form
 			end
