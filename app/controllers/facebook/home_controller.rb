c60f3b64a58a8b17fd839d633ba0dc4431159b01
Keith Gable <ziggy@ignition-project.com>
2008-12-30 05:05:52 +0000

    Setup breadcrumbs unnecessary
    git-svn-id: http://svn.macrodeck.com/macrodeck/trunk@1259 22037474-b016-0410-8482-8abe5be14c89

--- app/controllers/facebook/home_controller.rb
+++ app/controllers/facebook/home_controller.rb
@@ -1,8 +1,6 @@
 class Facebook::HomeController < ApplicationController
 	before_filter :require_facebook_login
 	before_filter :initialize_facebook_user
-	before_filter :setup_breadcrumbs
-
 	layout "facebook/home"
 
 	def index
