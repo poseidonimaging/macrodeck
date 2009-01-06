5f69bec136653888d2bbddd1137afd4205a76230
Keith Gable <ziggy@ignition-project.com>
2009-01-06 06:19:40 +0000

    Working mini-feed
    git-svn-id: http://svn.macrodeck.com/macrodeck/trunk@1282 22037474-b016-0410-8482-8abe5be14c89

--- app/controllers/facebook/home_controller.rb
+++ app/controllers/facebook/home_controller.rb
@@ -4,5 +4,43 @@ class Facebook::HomeController < ApplicationController
 	layout "facebook/home"
 
 	def index
+		@minifeed = []
+		@minifeed += minifeed_nudges_incoming unless minifeed_nudges_incoming.nil?
+		@minifeed.sort!.reverse![0..10]
 	end
+
+	private
+		def minifeed_nudges_incoming
+			minifeed_items = []
+			nudges = Relationship.find(:all,
+									   :conditions => ["(target_uuid = ? AND subject IS NOT NULL AND relationship = 'nudged') OR (source_uuid = ? AND subject IS NULL AND relationship = 'nudged')",
+															@fbuser.uuid, @fbuser.uuid],
+									   :order => "updated_at DESC")
+			if !nudges.nil? && nudges.length > 0
+				nudges.each do |nudge|
+					# Old nudge style lacked a sender thus we don't know who sent the nudge, but we can still show it
+					if nudge.subject.nil?
+						begin
+							event = Event.find_by_uuid(nudge.target_uuid)
+							feed_item_text = "You were nudged about <a href='#{event.url(:facebook => true)}'>#{event.summary}</a>"
+							minifeed_items << [ nudge.updated_at, feed_item_text ]
+						rescue
+							nil # we won't do anything if any of the previous failed
+						end
+					else
+						begin
+							event = Event.find_by_uuid(nudge.subject)
+							nudger = User.find_by_uuid(nudge.source_uuid)
+							feed_item_text = "<fb:name uid='#{nudger.facebook_uid}' linked='true' ifcantsee='A user' subjectid='#{@fbuser.facebook_uid}' /> nudged you about
+												<a href='#{event.url(:facebook => true)}'>#{event.summary}</a>"
+							minifeed_items << [ nudge.updated_at, feed_item_text ]
+						rescue
+							nil # We don't do anything
+						end
+					end
+				end
+			end
+
+			return minifeed_items
+		end
 end
