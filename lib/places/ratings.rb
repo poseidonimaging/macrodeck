b652f3b09c4e7fee6850dcb2c2a37934d322abd3
Keith Gable <ziggy@ignition-project.com>
2009-03-23 05:36:40 +0000

    Fix a logic error, add methods for experience ratings
    git-svn-id: http://svn.macrodeck.com/macrodeck/trunk@1349 22037474-b016-0410-8482-8abe5be14c89

--- lib/places/ratings.rb
+++ lib/places/ratings.rb
@@ -12,7 +12,7 @@ module MacroDeck
 						relationship_exists = Relationship.find(:first, :conditions => ["source_uuid = ? AND target_uuid = ? AND relationship IN ('like','dislike')", @fbuser.uuid, @place.uuid])
 
 						# If they're able to change their mind, let them.
-						if relationship_exists && 60.days.since(my_rating.updated_at) <= Time.now
+						if relationship_exists && 60.days.since(relationship_exists.updated_at) <= Time.now
 							relationship_exists.destroy
 							relationship_exists = nil
 						end
@@ -41,6 +41,7 @@ module MacroDeck
 					redirect_to @place.url(:facebook => true)
 				end
 			end
+
 			def dislike
 				if params[:place]
 					@place = Place.find_by_uuid(params[:place])
@@ -48,7 +49,7 @@ module MacroDeck
 						relationship_exists = Relationship.find(:first, :conditions => ["source_uuid = ? AND target_uuid = ? AND relationship IN ('like','dislike')", @fbuser.uuid, @place.uuid])
 						
 						# If they're able to change their mind, let them.
-						if relationship_exists && 60.days.since(my_rating.updated_at) <= Time.now
+						if relationship_exists && 60.days.since(relationship_exists.updated_at) <= Time.now
 							relationship_exists.destroy
 							relationship_exists = nil
 						end
@@ -72,6 +73,70 @@ module MacroDeck
 							}
 							feed_tokens = feed_tokens.to_json
 							fbsession.feed_publishUserAction( :template_bundle_id => FB_FEED_RATING, :template_data => feed_tokens )
+						end
+					end
+					redirect_to @place.url(:facebook => true)
+				end
+			end
+
+			def good_experience
+				if params[:place]
+					@place = Place.find_by_uuid(params[:place])
+					if @place != nil
+						relationship_exists = Relationship.find(:first,	:conditions => ["source_uuid = ? AND target_uuid = ? AND relationship IN ('good_experience','bad_experience')", @fbuser.uuid, @place.uuid],
+																		:order => "updated_at DESC")
+
+						if relationship_exists.nil? || (relationship_exists && 7.days.since(relationship_exists.updated_at) <= Time.now)
+							new_rel = Relationship.new do |r|
+								r.source_uuid = @fbuser.uuid
+								r.target_uuid = @place.uuid
+								r.relationship = "good_experience"
+							end
+							new_rel.save!
+
+							# Now publish this to their feed.
+							#feed_tokens = {
+							#	"place"			=> @place.name,
+							#	"placeurl"		=> @place.url,
+							#	"type"			=> @place.place_metadata.place_type_to_s,
+							#	"placeinfo"		=> @place.description,
+							#	"location"		=> "#{@place.city.name}, #{@place.city.state(:abbreviation => true)}",
+							#	"thumb"			=> "thumbs up"
+							#}
+							#feed_tokens = feed_tokens.to_json
+							#fbsession.feed_publishUserAction( :template_bundle_id => FB_FEED_RATING, :template_data => feed_tokens )
+						end
+					end
+					redirect_to @place.url(:facebook => true)
+				end
+			end
+
+			def bad_experience
+				if params[:place]
+					@place = Place.find_by_uuid(params[:place])
+					if @place != nil
+						relationship_exists = Relationship.find(:first,	:conditions => ["source_uuid = ? AND target_uuid = ? AND relationship IN ('good_experience','bad_experience')", @fbuser.uuid, @place.uuid],
+																		:order => "updated_at DESC")
+
+						if relationship_exists.nil? || (relationship_exists && 7.days.since(relationship_exists.updated_at) <= Time.now)
+							new_rel = Relationship.new do |r|
+								r.source_uuid = @fbuser.uuid
+								r.target_uuid = @place.uuid
+								r.relationship = "bad_experience"
+							end
+							new_rel.save!
+
+							# Now publish this to their feed.
+							#feed_tokens = {
+							#	"place"			=> @place.name,
+							#	"placeurl"		=> @place.url,
+							#	"type"			=> @place.place_metadata.place_type_to_s,
+							#	"placeinfo"		=> @place.description,
+							#	"location"		=> "#{@place.city.name}, #{@place.city.state(:abbreviation => true)}",
+							#	"thumb"			=> "thumbs down"
+							#}
+							#feed_tokens = feed_tokens.to_json
+							#fbsession.feed_publishUserAction( :template_bundle_id => FB_FEED_RATING, :template_data => feed_tokens )
 						end
 					end
 					redirect_to @place.url(:facebook => true)
