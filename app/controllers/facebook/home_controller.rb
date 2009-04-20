e8e935af123f0db9f5e9c2bf070fc4e84cd32fc8
Keith Gable <ziggy@ignition-project.com>
2009-04-20 05:10:11 +0000

    show cached content I think
    git-svn-id: http://svn.macrodeck.com/macrodeck/trunk@1360 22037474-b016-0410-8482-8abe5be14c89

--- app/controllers/facebook/home_controller.rb
+++ app/controllers/facebook/home_controller.rb
@@ -8,10 +8,18 @@ class Facebook::HomeController < ApplicationController
 
 	def index
 		# Build the recommendations box.
+		if HtmlPart.find(:first, :conditions => ["user_id = ? AND urn = 'macrodeck/home/minifeed.fbml'", @fbuser.id])
+			@minifeed_content = HtmlPart.find(:first, :conditions => ["user_id = ? AND urn = 'macrodeck/home/minifeed.fbml'", @fbuser.id]).content
+		else
+			@minifeed_content = "<p style='text-align: center;'>There are currently no entries in your minifeed.</p>"
+		end
 
+		if HtmlPart.find(:first, :conditions => ["user_id = ? AND urn = 'macrodeck/home/recommendations.fbml'", @fbuser.id])
+			@recommendations_content = HtmlPart.find(:first, :conditions => ["user_id = ? AND urn = 'macrodeck/home/recommendations.fbml'", @fbuser.id]).content
+		else
+			@recommendations_content = "<p style='text-align: center;'>There are currently no recommendations to show.</p>"
+		end
 	end
-
-
 
 	private
 		# XXX: This needs to move to PlaceMetadata and stay there...
