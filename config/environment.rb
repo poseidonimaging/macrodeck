afa00feb2c7ea39657b7a967b3415a5d6bf9372d
Keith Gable <ziggy@ignition-project.com>
2009-07-12 19:09:22 +0000

    Update from Rails 2.0.2 to Rails 2.3.2 in order to use new features in Rails. Also use config.gem to load a Gem
    git-svn-id: http://svn.macrodeck.com/macrodeck/branches/places-20070719@1380 22037474-b016-0410-8482-8abe5be14c89

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@
 #ENV['RAILS_ENV'] ||= 'development'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.0.2'
+RAILS_GEM_VERSION = '2.3.2'
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -40,6 +40,8 @@ Rails::Initializer.run do |config|
   # config.active_record.default_timezone = :utc
   
   # See Rails::Configuration for more options
+
+  config.gem 'geokit'
 end
 
 # Add new inflection rules using the following format 
