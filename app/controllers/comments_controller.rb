6ae9f8d4731b39ea82efa88a2d9b2dbf2f28ba63
Keith Gable <ziggy@ignition-project.com>
2009-12-13 21:24:45 +0000

    Add (initial) CommentsController to handle wall posts.
    git-svn-id: http://svn.macrodeck.com/macrodeck/trunk@1439 22037474-b016-0410-8482-8abe5be14c89

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,111 @@
+# This controller handles comments.
+class CommentsController < ApplicationController
+	layout 'default'
+	before_filter :find_country
+	before_filter :find_state
+	before_filter :find_city
+	before_filter :find_place	# may not be used; only if comments exist on an event/calendar.
+	before_filter :find_calendar	# may not be used; only if comments exist on an event/calendar.
+	before_filter :find_event	# may not be used; only if comments exist on an event/calendar.
+	before_filter :find_wall
+	before_filter :find_comment
+
+	# List all comments
+	def index
+		@comments = @wall.comments
+		
+		respond_to do |format|
+			format.html # index.html.erb 
+		end
+	end
+
+	# This shows a specific comment.
+	# FIXME: Implement this.
+	def show
+		raise NotImplementedError, "FIXME: Implement showing comments."
+	end
+	
+	# Shows the interface for editing a comment.
+	def edit
+		raise NotImplementedError, "FIXME: Implement editing comments."
+		#respond_to do |format|
+		#	format.html # edit.html.erb
+		#end
+	end
+	
+	# Handles updating a comment
+	# FIXME: Implement this.
+	def update
+		raise NotImplementedError, "FIXME: Implement editing comments."
+		
+=begin
+		if @event.update_attributes(params[:event])
+			if @place.nil?
+				respond_to do |format|
+					format.html { redirect_to(country_state_city_event_path(params[:country_id], params[:state_id], params[:city_id], params[:id])) }
+					format.xml  { head :ok }
+				end
+			else
+				respond_to do |format|
+					format.html { redirect_to(country_state_city_place_event_path(params[:country_id], params[:state_id], params[:city_id], params[:place_id], params[:id])) }
+					format.xml  { head :ok }
+				end
+			end
+		else
+			respond_to do |format|
+				format.html { render :action => "edit" }
+				format.xml  { render :xml => @event.errors, :status => :unprocessable_entity }
+			end
+		end
+=end
+	end
+	
+	# Handles creating a comment.
+	def create
+	end
+
+	private
+		# Finds the country associated with the request.
+		def find_country
+			@country = Category.find_by_parent_uuid_and_url_part(Category.find(:first, :conditions => ["parent_uuid IS NULL AND url_part = ?", "places"]).uuid, params[:country_id].downcase) if params[:country_id]
+		end
+
+		# Finds the state associated with the request.
+		def find_state
+			@state = Category.find_by_parent_uuid_and_url_part(@country.uuid, params[:state_id].downcase) if params[:country_id] && params[:state_id]
+		end
+
+		# Finds the city associated with the request
+		def find_city
+			if params[:country_id] && params[:state_id] && params[:city_id]
+				# Walk the tree 
+				city_category = Category.find_by_parent_uuid_and_url_part(@state.uuid, params[:city_id].downcase)
+				@city = City.find(:first, :conditions => { :category_id => city_category.id })
+			end
+		end
+
+		# Finds the place associated with the request (if any)
+		def find_place
+			if params[:country_id] && params[:state_id] && params[:city_id] && params[:place_id] && params[:place_id].length > 0
+				@place = Place.find(:first, :conditions => ["url_part = ? OR uuid = ?", params[:place_id], params[:place_id]])
+			end
+		end
+		
+		# Finds the calendar associated with the request
+		def find_calendar
+			if @place.nil?
+				# We need to get the city's calendar.
+				@calendar = @city.calendar
+			else
+				@calendar = @place.calendar
+			end
+		end
+
+		# Finds the event associated with the request (if any)
+		def find_event
+			if params[:id] && params[:id].length > 0
+				@event = Event.find(:first, :conditions => ["url_part = ? OR uuid = ?", params[:id], params[:id]])
+			end
+		end
+end
+
