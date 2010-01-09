f529fad44e8b5dfe7dcf8cddbd55f90c8879c346
Keith Gable <ziggy@ignition-project.com>
2010-01-09 22:12:26 +0000

    get_wall_path updated to work on city/place/calendar/event pages
    git-svn-id: http://svn.macrodeck.com/macrodeck/trunk@1457 22037474-b016-0410-8482-8abe5be14c89

--- app/helpers/walls_helper.rb
+++ app/helpers/walls_helper.rb
@@ -1,13 +1,22 @@
 # This helper has wall functions in it.
 module WallsHelper
 	def get_wall_path
-		# FIXME: params[:id] needs to map to whatever it's supposed to below.
 		country		= params[:country_id]
 		state		= params[:state_id]
 		city		= params[:city_id]
 		place		= params[:place_id]
 		calendar	= params[:calendar_id]
 		event		= params[:event_id]
+
+		# If we're on the page with the wall, params[:id] will instead map to
+		# the event, place, city, or calendar we need.
+		unless params[:controller].nil?
+			case params[:controller]
+			when "city"		then city = params[:id]
+			when "place"	then place = params[:id]
+			when "calendar"	then calendar = params[:id]
+			when "event"	then event = params[:id]
+		end
 
 		# These three will always not be nil.
 		if !country.nil? && !state.nil? && !city.nil?
