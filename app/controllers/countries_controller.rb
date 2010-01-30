1574914cf9292fa826c6059b1aeb9c6424495f38
Keith Gable <ziggy@ignition-project.com>
2010-01-30 23:21:57 +0000

    Style Countries in the new UI
    git-svn-id: http://svn.macrodeck.com/macrodeck/trunk@1482 22037474-b016-0410-8482-8abe5be14c89

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -2,10 +2,11 @@
 # A country is a category whose immediate parent is "MacroDeck Places".
 # Really this is a stub because the interface for categories behaves differently.
 class CountriesController < ApplicationController
-	layout 'default'
+	layout 'restlessnapkin'
 
 	# List countries
 	def index
+		@page_title = "Countries"
 		@countries = Category.find(:all, :conditions => ["parent_uuid = ?", Category.find(:first, :conditions => ["parent_uuid IS NULL AND url_part = ?", "places"]).uuid])
 	end
 
