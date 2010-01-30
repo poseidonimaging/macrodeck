d7996e3163c529ea061835429faa891607dcc4ef
Keith Gable <ziggy@ignition-project.com>
2010-01-30 23:57:24 +0000

    Use the restlessnapkin layout, specify a page title.
    git-svn-id: http://svn.macrodeck.com/macrodeck/trunk@1492 22037474-b016-0410-8482-8abe5be14c89

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -2,10 +2,11 @@
 # A state is a category whose parent is a country
 # Really this is a stub because the interface for categories behaves differently.
 class StatesController < ApplicationController
-	layout 'default'
+	layout 'restlessnapkin'
 
 	# List states
 	def index
+		@page_title = "#{@country.name} > States"
 		@country = Category.find_by_parent_uuid_and_url_part(Category.find(:first, :conditions => ["parent_uuid IS NULL AND url_part = ?", "places"]).uuid, params[:country_id].downcase)
 		@states = @country.children
 	end
