17200033c3366acecf34533db652843a89151211
Keith Gable <ziggy@ignition-project.com>
2010-07-17 22:28:16 +0000

    Remove all of the old stuff, upgrade to latest Rails. I notice that I don't have initializers, so I might benefit from regenerating the app and copying the new stuff over.
    git-svn-id: http://svn.macrodeck.com/macrodeck/trunk@1588 22037474-b016-0410-8482-8abe5be14c89

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@
 #ENV['RAILS_ENV'] ||= 'development'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5'
+RAILS_GEM_VERSION = '2.3.8'
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -15,7 +15,7 @@ Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence those specified here
   
   # Skip frameworks you're not going to use
-  # config.frameworks -= [ :action_web_service, :action_mailer ]
+  config.frameworks -= [ :active_record ]
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
@@ -64,77 +64,3 @@ end
 # end
 
 # Include your application configuration below
-
-# A list of months!
-MONTHS_EN = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"] 
-
-# E-mail validation regex
-#EMAIL_VALIDATION   = /^[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/
-EMAIL_VALIDATION    = /^([a-zA-Z0-9&_?\/`!|#*$^%=~{}+'-]+|"([\x00-\x0C\x0E-\x21\x23-\x5B\x5D-\x7F]|\\[\x00-\x7F])*")(\.([a-zA-Z0-9&_?\/`!|#*$^%=~{}+'-]+|"([\x00-x0C\x0E-\x21\x23-\x5B\x5D-\x7F]|\\[\x00-\x7F])*"))*@([a-zA-Z0-9&_?\/`!|#*$^%=~{}+'-]+|\[([\x00-\x0C\x0E-\x5A\x5E-\x7F]|\\[\x00-\x7F])*\])(\.([a-zA-Z0-9&_?\/`!|#*$^%=~{}+'-]+|\[([\x00-\x0C\x0E-\x5A\x5E-\x7F]|\\[\x00-\x7F])*\]))*$/
-PHONE_VALIDATION	= /^[01]?[- .]?(\([2-9]\d{2}\)|[2-9]\d{2})[- .]?\d{3}[- .]?\d{4}$/
-
-# User Password Salt
-PASSWORD_SALT   = "giomullyoxonoind" # Random character generator
-
-# Places
-PLACES_TEST_SERVER = true
-
-if PLACES_TEST_SERVER
-	PLACES_BASEURL = "http://places.intranet.ignition-project.com:3000" # do not use a trailing slash
-	PLACES_FBURL = "http://apps.facebook.com/macrodeckplaces-test" # do not use a trailing slash
-	PLACES_APPURL = "http://www.facebook.com/apps/application.php?id=6592864647"
-	FLICKR_API_KEY = "686693e936d2bd4bfc3c5477fa3f1332"
-	FLICKR_SECRET = "b9a60cf2f27ed59c"
-	ENV['RAILS_ENV'] ||= 'development'
-	ActionController::Base.asset_host = PLACES_BASEURL
-
-	FB_FEED_ATTENDING = "33152039647"
-	FB_FEED_IS_PATRON = "46850634647"
-	FB_FEED_RATING = "55536119647"
-
-	puts "*** Using Test Server!"
-else
-	PLACES_BASEURL = "http://places.macrodeck.com" # do not use a trailing slash
-	PLACES_FBURL = "http://apps.facebook.com/macrodeckplaces" # do not use a trailing slash
-	PLACES_APPURL = "http://www.facebook.com/apps/application.php?id=2475497610"
-	FLICKR_API_KEY = "686693e936d2bd4bfc3c5477fa3f1332"
-	FLICKR_SECRET = "b9a60cf2f27ed59c"
-	ActionController::Base.asset_host = PLACES_BASEURL
-
-	FB_FEED_ATTENDING = "39109862610"
-	FB_FEED_IS_PATRON = "51769422610"
-	FB_FEED_RATING = "63310062610"
-end
-
-# Time/date formats
-Time::DATE_FORMATS[:human] = lambda { |time| time.strftime("%B #{time.day.ordinalize}, %Y %I:%M%p") }
-
-
-# Configure UltraSphinx
-Ultrasphinx::Search.excerpting_options = HashWithIndifferentAccess.new({
-	:before_match => "<span class='highlight'>",
-	:after_match => "</span>",
-	:chunk_seperator => "...",
-	:limit => 256,
-	:around => 3,
-	:content_methods => ['description', 'data']
-})
-
-# Start services we need
-# Configuration until we do dependency stuff.
-Services.startService "uuid_service"
-Services.startService "data_service"
-Services.startService "user_service"
-Services.startService "wall_service"
-Services.startService "event_service"
-Services.startService "places_service"
-Services.startService "navigation_service"
-Services.startService "content_cache_service"
-
-# Start web services now
-#Services.startService "data_web_service"
-
-require 'flickr'
-require 'flickr_extensions'
-require 'string_extension'
-require 'sparklines'
