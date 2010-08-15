a42718e7f1fdaaa952630d1b092f5b9ba606d422
Keith Gable <ziggy@ignition-project.com>
2010-08-15 00:48:48 +0000

    Add a method to render a 404 page. From <http://stackoverflow.com/questions/2385799/how-to-redirect-to-a-404-in-rails>
    git-svn-id: http://svn.macrodeck.com/macrodeck/trunk@1686 22037474-b016-0410-8482-8abe5be14c89

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,4 +21,16 @@ class ApplicationController < ActionController::Base
 	headers["Cache-Control"] = "no-cache, must-revalidate"
 	headers["Pragma"] = "no-cache"
     end
+
+    def render_404(exception = nil)
+	if exception
+	    logger.info "Rendering 404 with exception: #{exception.message}"
+	end
+
+	respond_to do |format|
+	    format.html { render :file => "#{Rails.root}/public/404.html", :status => :not_found }
+	    format.xml  { head :not_found }
+	    format.any  { head :not_found }
+	end
+    end
 end
