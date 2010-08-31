45d3e081f94be12a166bbf140a8285070c689f64
Keith Gable <ziggy@ignition-project.com>
2010-08-31 22:07:50 +0000

    Make the back button take you to page 1 when you're on not page 1.
    git-svn-id: http://svn.macrodeck.com/macrodeck/trunk@1806 22037474-b016-0410-8482-8abe5be14c89

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -24,7 +24,11 @@ class EventsController < ApplicationController
 		@events_count = 0
 	    end
 	    #@back_button = [@region.title, country_region_localities_path(@country.id, @region.id)]
-	    @back_button = [@locality.title, country_region_locality_path(@country.id, @region.id, @locality.id)]
+	    if @start_item == 0
+		@back_button = [@locality.title, country_region_locality_path(@country.id, @region.id, @locality.id)]
+	    else
+		@back_button = [@locality.title, country_region_locality_events_path(@country.id, @region.id, @locality.id)]
+	    end
 	end
 	respond_to do |format|
 	    format.html # index.html.erb
