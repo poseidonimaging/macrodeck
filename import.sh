83ff8f30728bcf4bebbb980083ba691e044dbf0b
Keith Gable <ziggy@ignition-project.com>
2010-09-17 03:06:04 +0000

    Also load shipped objects.
    git-svn-id: http://svn.macrodeck.com/macrodeck/trunk@1845 22037474-b016-0410-8482-8abe5be14c89

--- import.sh
+++ import.sh
@@ -1,11 +1,14 @@
 #/bin/sh
+cd vendor/plugins/macrodeck-platform
+rake macrodeck:load_shipped_objects DB="macrodeck-${RAILS_ENV}"
+cd ../../..
 
-rake macrodeck:import:countries COUNTRIES="db/data/countries.tsv"
-rake macrodeck:import:regions REGIONS="db/data/us_states.tsv"
-rake macrodeck:import:localities LOCALITIES="db/data/ok_cities.tsv"
-rake macrodeck:import:localities LOCALITIES="db/data/tx_cities.tsv"
-rake macrodeck:import:neighborhoods NEIGHBORHOODS="db/data/tulsa_neighborhoods.tsv"
-rake macrodeck:import:neighborhoods NEIGHBORHOODS="db/data/austin_neighborhoods.tsv"
-rake macrodeck:import:places PLACES="db/data/tulsa_places.tsv"
-rake macrodeck:import:places PLACES="db/data/austin_places.tsv"
+rake macrodeck:import:countries COUNTRIES="db/data/countries.tsv" &&
+rake macrodeck:import:regions REGIONS="db/data/us_states.tsv" &&
+rake macrodeck:import:localities LOCALITIES="db/data/ok_cities.tsv" &&
+rake macrodeck:import:localities LOCALITIES="db/data/tx_cities.tsv" &&
+rake macrodeck:import:neighborhoods NEIGHBORHOODS="db/data/tulsa_neighborhoods.tsv" &&
+rake macrodeck:import:neighborhoods NEIGHBORHOODS="db/data/austin_neighborhoods.tsv" &&
+rake macrodeck:import:places PLACES="db/data/tulsa_places.tsv" &&
+rake macrodeck:import:places PLACES="db/data/austin_places.tsv" &&
 rake macrodeck:import:events EVENTS="db/data/austin_events.tsv"
