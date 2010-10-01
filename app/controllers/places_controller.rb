3e8512f5794cf85b49b881b068136b4eecab24a2
Keith Gable <ziggy@ignition-project.com>
2010-10-01 03:24:51 +0000

    Handle a zero resultset.
    git-svn-id: http://svn.macrodeck.com/macrodeck/trunk@1916 22037474-b016-0410-8482-8abe5be14c89

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -25,25 +25,29 @@ class PlacesController < ApplicationController
 	    startkey = @locality.path.dup.push(0)
 	    endkey = @locality.path.dup.push({})
 	    @places = Place.view("by_path_without_neighborhood_alpha", :reduce => false, :startkey => startkey, :endkey => endkey, :limit => 10, :skip => @start_item)
-	    @places_count = Place.view("by_path_without_neighborhood_alpha", :reduce => true, :startkey => startkey, :endkey => endkey)["rows"][0]["value"]
+	    count_query = Place.view("by_path_without_neighborhood_alpha", :reduce => true, :startkey => startkey, :endkey => endkey)
+	    @places_count = count_query["rows"].length == 0 ? 0 : count_query["rows"][0]["value"]
 	    #@back_button = [@region.title, country_region_localities_path(params[:country_id], params[:region_id])]
 	elsif params[:fare].nil? && !params[:neighborhood].nil?
 	    startkey = @locality.path.dup.push(params[:neighborhood]).push(0)
 	    endkey = @locality.path.dup.push(params[:neighborhood]).push({})
 	    @places = Place.view("by_path_alpha", :reduce => false, :startkey => startkey, :endkey => endkey, :limit => 10, :skip => @start_item)
-	    @places_count = Place.view("by_path_alpha", :reduce => true, :startkey => startkey, :endkey => endkey)["rows"][0]["value"]
+	    count_query = Place.view("by_path_alpha", :reduce => true, :startkey => startkey, :endkey => endkey)
+	    @places_count = count_query["rows"].length == 0 ? 0 : count_query["rows"][0]["value"]
 	    @back_button = [@locality.title, country_region_locality_places_path(params[:country_id], params[:region_id], params[:id])]
 	elsif !params[:fare].nil? && params[:neighborhood].nil?
 	    startkey = @locality.path.dup.push(params[:fare]).push(0)
 	    endkey = @locality.path.dup.push(params[:fare]).push({})
 	    @places = Place.view("by_fare_alpha", :reduce => false, :startkey => startkey, :endkey => endkey, :limit => 10, :skip => @start_item)
-	    @places_count = Place.view("by_fare_alpha", :reduce => true, :startkey => startkey, :endkey => endkey)["rows"][0]["value"]
+	    count_query = Place.view("by_fare_alpha", :reduce => true, :startkey => startkey, :endkey => endkey)
+	    @places_count = count_query["rows"].length == 0 ? 0 : count_query["rows"][0]["value"]
 	    @back_button = [@locality.title, country_region_locality_places_path(params[:country_id], params[:region_id], params[:id])]
 	elsif !params[:fare].nil? && !params[:neighborhood].nil?
 	    startkey = @locality.path.dup.push(params[:neighborhood]).push(params[:fare]).push(0)
 	    endkey = @locality.path.dup.push(params[:neighborhood]).push(params[:fare]).push({})
 	    @places = Place.view("by_fare_alpha", :reduce => false, :startkey => startkey, :endkey => endkey, :limit => 10, :skip => @start_item)
-	    @places_count = Place.view("by_fare_alpha", :reduce => true, :startkey => startkey, :endkey => endkey)["rows"][0]["value"]
+	    count_query = Place.view("by_fare_alpha", :reduce => true, :startkey => startkey, :endkey => endkey)
+	    @places_count = count_query["rows"].length == 0 ? 0 : count_query["rows"][0]["value"]
 	    @back_button = [@locality.title, country_region_locality_places_path(params[:country_id], params[:region_id], params[:id])]
 	end
 
