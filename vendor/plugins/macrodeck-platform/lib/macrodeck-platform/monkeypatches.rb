670743edc0a1393a168b142f7bda1258f49772fe
Keith Gable <ziggy@ignition-project.com>
2010-10-13 03:11:54 +0000

    Update to Git HEAD
    git-svn-id: http://svn.macrodeck.com/macrodeck/trunk@1961 22037474-b016-0410-8482-8abe5be14c89

--- vendor/plugins/macrodeck-platform/lib/macrodeck-platform/monkeypatches.rb
+++ vendor/plugins/macrodeck-platform/lib/macrodeck-platform/monkeypatches.rb
@@ -1,14 +1,14 @@
 # This monkey patch is needed because for some reason, CouchDB requires
 # reduction parameters to be at the beginning of the query string
 
-def CouchRest
+module CouchRest
 	class << self
 		def paramify_url url, params = {}
 			if params && !params.empty?
 				prequery = []
-				prequery << "reduce=#{CGI.escape(params.delete[:reduce].to_s)}" if params.include? :reduce
-				prequery << "group=#{CGI.escape(params.delete[:group].to_s)}" if params.include? :group
-				prequery << "group_level=#{CGI.escape(params.delete[:group_level].to_s)}" if params.include? :group_level
+				prequery << "reduce=#{CGI.escape(params.delete(:reduce).to_s)}" if params.include? :reduce
+				prequery << "group=#{CGI.escape(params.delete(:group).to_s)}" if params.include? :group
+				prequery << "group_level=#{CGI.escape(params.delete(:group_level).to_s)}" if params.include? :group_level
 				prequery = prequery.join("&")
 				prequery << "&" if prequery.length > 0 # needed so that query and prequery go together.
 				query = params.collect do |k,v|
