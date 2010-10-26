a7abbf17cbaef51c8e261d8c0563b203ccd61608
Keith Gable <ziggy@ignition-project.com>
2010-10-26 03:23:55 +0000

    Make the query actually be a boolean AND.
    git-svn-id: http://svn.macrodeck.com/macrodeck/trunk@1972 22037474-b016-0410-8482-8abe5be14c89

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -23,7 +23,7 @@ class PlacesController < ApplicationController
 
 	if !params[:q].nil?
 	    @page_title_log = "#{@locality.title} > Places > Search"
-	    query = "path:#{@locality.path.join("/")}/* AND (#{params[:q]})"
+	    query = "path:#{@locality.path.join("/")}/* AND (#{query_to_boolean_and(params[:q])})"
 	    place_search = Place.search("common_fields", query, :limit => 10, :skip => @start_item)
 	    @places = place_search["rows"]
 	    @places_count = place_search["rows"].length == 0 ? 0 : place_search["total_rows"]
