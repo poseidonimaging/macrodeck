585b73923d31d9d8a9326f83783997d812767a10
Keith Gable <ziggy@ignition-project.com>
2010-12-16 03:07:51 +0000

    Use live instead of click so that the button works when loading more crap.
    git-svn-id: http://svn.macrodeck.com/macrodeck/trunk@2074 22037474-b016-0410-8482-8abe5be14c89

--- public/javascripts/mobile.js
+++ public/javascripts/mobile.js
@@ -20,7 +20,7 @@ jQuery(document).ready(function() {
     }
 
     // Hook the next button if possible
-    jQuery(".nextbtn a").click(function(e) {
+    jQuery(".nextbtn a").live('click', function(e) {
 	e.preventDefault();
 
 	jQuery.get(jQuery(".nextbtn a").attr("href"), function(data) {
